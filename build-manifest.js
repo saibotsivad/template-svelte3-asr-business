5eba95e8f18aa93e24d6743158c516cdf7b02d71
Tobias Davis <accounts@davistobias.com>
2019-08-10 15:24:00 -0500

    fix: straighten out the favicon situation, but it's not tested

--- build-manifest.js
+++ build-manifest.js
@@ -18,3 +18,20 @@ writeFileSync(
 		2
 	),
 	{ encoding: 'utf8' })
+
+const browserConfigTemplate = `<?xml version="1.0" encoding="utf-8"?>
+<browserconfig>
+    <msapplication>
+        <tile>
+            <square150x150logo src="asset/mstile-150x150.png"/>
+            <TileColor>${configuration.microsoftTileColor}</TileColor>
+        </tile>
+    </msapplication>
+</browserconfig>
+`
+
+writeFileSync(
+	'./public/browserconfig.xml',
+	browserConfigTemplate,
+	{ encoding: 'utf8' }
+)
--- configuration.js
+++ configuration.js
@@ -13,8 +13,15 @@ export default {
 	// When launched from a desktop shortcut, this
 	// color is used before assets are loaded.
 	backgroundColor: '#fff',
-	// TODO: what is this used for?
+	// Used by Android (possibly others) to set the
+	// task bar, depending on app display.
 	themeColor: '#ff3e00',
+	// Safari tabbed icons get monochromed, so you need a
+	// color that contrasts well. TODO: check this information.
+	safariTabColor: '#5bbad5',
+	// Background color of tile, when installed as a Windows
+	// desktop application.
+	microsoftTileColor: '#00aba9',
 	// TODO: either always true, or enable/disable for local development?
 	serviceWorker: true,
 	// If set, this will add the Twitter card metadata.
@@ -25,12 +32,12 @@ export default {
 	icons: [
 		// TODO add for other sizes, e.g. 72, 96, 144, 168
 		{
-			src: 'asset/icon-android-chrome-192.png',
+			src: 'asset/favicon/android-chrome-192.png',
 			sizes: '192x192',
 			type: 'image/png'
 		},
 		{
-			src: 'asset/icon-android-chrome-512.png',
+			src: 'asset/favicon/android-chrome-512.png',
 			sizes: '512x512',
 			type: 'image/png'
 		}
